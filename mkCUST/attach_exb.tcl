5a1bb49bce9802e668c88d336940a425fb06dfb6
dodo5575 <dodo5575@gmail.com>
2016-02-10 20:17:03 -0600

    update mkCUST/attach_exb.tcl

--- mkCUST/attach_exb.tcl
+++ mkCUST/attach_exb.tcl
@@ -128,7 +128,66 @@ resetpsf                           ;# Destroys any previous attempts
 psfcontext reset                   ;# Tosses out any previously declared topology files
 
 # alias
+# Here's for nucleics
+foreach suff {"" 3 5} {
+    pdbalias residue DA$suff ADE
+    pdbalias residue DT$suff THY
+    pdbalias residue DC$suff CYT
+    pdbalias residue DG$suff GUA
+    pdbalias residue A$suff ADE
+    pdbalias residue T$suff THY
+    pdbalias residue C$suff CYT
+    pdbalias residue G$suff GUA
+}
+foreach bp { GUA CYT ADE THY URA } {
+    pdbalias atom $bp "O5\*" O5'
+    pdbalias atom $bp "C5\*" C5'
+    pdbalias atom $bp "O4\*" O4'
+    pdbalias atom $bp "C4\*" C4'
+    pdbalias atom $bp "C3\*" C3'
+    pdbalias atom $bp "O3\*" O3'
+    pdbalias atom $bp "C2\*" C2'
+    pdbalias atom $bp "O2\*" O2'
+    pdbalias atom $bp "C1\*" C1'
+    pdbalias atom $bp  OP1   O1P
+    pdbalias atom $bp  OP2   O2P
+}
+pdbalias atom THY C7 C5M
+# protein
 pdbalias residue HIS HSE
+pdbalias atom ILE CD1 CD
+pdbalias atom SER HG HG1
+# Heme aliases
+pdbalias residue HEM HEME
+pdbalias atom HEME "N A" NA
+pdbalias atom HEME "N B" NB
+pdbalias atom HEME "N C" NC
+pdbalias atom HEME "N D" ND
+# Water aliases
+pdbalias residue HOH TIP3
+pdbalias atom TIP3 O OH2
+pdbalias atom TIP3 OW OH2
+# Ion aliases    
+pdbalias residue K POT
+pdbalias atom POT K POT
+pdbalias residue ICL CLA
+pdbalias atom ICL CL CLA
+pdbalias residue NA SOD
+pdbalias atom SOD NA SOD
+pdbalias residue CA CAL
+pdbalias atom CAL CA CAL
+# Else
+pdbalias atom ATP C1* C1'
+pdbalias atom ATP C2* C2'
+pdbalias atom ATP C3* C3'
+pdbalias atom ATP C4* C4'
+pdbalias atom ATP C5* C5'
+pdbalias atom ATP O2* O2'
+pdbalias atom ATP O3* O3'
+pdbalias atom ATP O4* O4'
+pdbalias atom ATP O5* O5'
+
+
 
 # Add topology files
 set parDir [file normalize [file join [lindex $argv 4] "topology"]]
